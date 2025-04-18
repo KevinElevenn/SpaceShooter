29eb39715dfdfb5a1397dcd03d4487130c043e9a
Kevin <gmodgame258@gmail.com>
2025-04-18 13:45:00 -0700

    added inputsystem package and 2 scripts to test out readable notes

---
+++ SpaceShooterByOTI/Assets/Scripts/Interactor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    public Transform InteractorSource;
+    public float InteractRange;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Interact))
+    }
+}
