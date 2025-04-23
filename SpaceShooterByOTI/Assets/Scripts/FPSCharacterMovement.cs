71223906799695ff2d9a5799cf99ce2b2ae249d5
Keshav Iyer <keshaviyer1997@gmail.com>
2025-04-23 14:47:40 -0700

    did somethn

---
+++ SpaceShooterByOTI/Assets/Scripts/FPSCharacterMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using CharacterMovement;
+public class FPSCharacterMovement : CharacterMovement3D
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
